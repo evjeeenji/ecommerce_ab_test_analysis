dcc1f865575b6c5fdb3cdd4fb200f991227f14a9
evjeeenji <zhmenichka.panova@gmail.com>
2026-02-16 15:58:37 +0200

    ab_test_dataset.sql

--- ab_test_dataset.sql
+++ ab_test_dataset.sql
@@ -3,18 +3,19 @@ SELECT
   test_group,
   COUNT(DISTINCT user_id) as users,
   COUNTIF(event_name = 'purchase') as orders,
-  -- Revenue для розрахунку ROI
+  -- Revenue
   SUM(ecommerce.purchase_revenue) as total_revenue
 FROM (
   SELECT 
     event_date,
     user_pseudo_id as user_id,
     ecommerce, -- структура з даними про дохід
-    -- Імітація спліта 50/50
+    -- Імітація розділення 50/50
     IF(MOD(ABS(FARM_FINGERPRINT(user_pseudo_id)), 2) = 0, 'Control', 'Test') as test_group,
     event_name
   FROM `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
   WHERE _TABLE_SUFFIX BETWEEN '20201101' AND '20210131' -- Обмежимо період (3 місяці), щоб швидше рахувало
 )
 GROUP BY 1, 2
+
 ORDER BY 1, 2
